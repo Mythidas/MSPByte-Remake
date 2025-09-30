2f6adba9dc92478e3674c25498e7c224c399ec77
Mythidas <root@mythidas.com>
2025-09-30 02:23:25 -0500

    add error handling and logging to ticket create route

--- backend/agents/src/api/v1.0/ticket/create/index.ts
+++ backend/agents/src/api/v1.0/ticket/create/index.ts
@@ -1,153 +1,202 @@
 import { HaloPSAConnector } from "@workspace/shared/lib/connectors/HaloPSAConnector.js";
 import { getRow } from "@workspace/shared/lib/db/orm.js";
 import Debug from "@workspace/shared/lib/Debug.js";
-import Encryption from "@workspace/shared/lib/Encryption.js";
 import { generateUUID } from "@workspace/shared/lib/utils.js";
 import { HaloPSAConfig } from "@workspace/shared/types/integrations/halopsa/index.js";
 import { FastifyInstance } from "fastify";
 
 export default async function (fastify: FastifyInstance) {
   fastify.post("/", async (req) => {
-    const siteID = req.headers["x-site-id"] as string;
-    const deviceID = req.headers["x-device-id"] as string;
-    if (!siteID || !deviceID) {
-      return Debug.response(
-        {
-          error: {
-            module: "v1.0/ticket/create",
-            context: "POST",
-            message: "API headers invalid",
-            code: "401",
+    try {
+      const siteID = req.headers["x-site-id"] as string;
+      const deviceID = req.headers["x-device-id"] as string;
+      if (!siteID || !deviceID) {
+        return Debug.response(
+          {
+            error: {
+              module: "v1.0/ticket/create",
+              context: "POST",
+              message: "API headers invalid",
+              code: "401",
+            },
           },
-        },
-        401
-      );
-    }
+          401
+        );
+      }
 
-    const [{ data: agent }, { data: site }] = await Promise.all([
-      getRow("agents", {
-        filters: [["id", "eq", deviceID]],
-      }),
-      getRow("sites", {
-        filters: [["id", "eq", siteID]],
-      }),
-    ]);
+      const [{ data: agent }, { data: site }] = await Promise.all([
+        getRow("agents", {
+          filters: [["id", "eq", deviceID]],
+        }),
+        getRow("sites", {
+          filters: [["id", "eq", siteID]],
+        }),
+      ]);
 
-    // TODO: Hard coded for current implementation... Will need a tenant settings or site settings to determine proper PSA
-    const { data: dataSource } = await getRow("data_sources", {
-      filters: [
-        ["integration_id", "eq", "halopsa"],
-        ["site_id", "eq", generateUUID(true)],
-      ],
-    });
+      // TODO: Hard coded for current implementation... Will need a tenant settings or site settings to determine proper PSA
+      const { data: dataSource } = await getRow("data_sources", {
+        filters: [
+          ["integration_id", "eq", "halopsa"],
+          ["site_id", "eq", generateUUID(true)],
+        ],
+      });
 
-    if (!dataSource || !site || !agent) {
-      return Debug.response(
-        {
-          error: {
-            module: "v1.0/ticket/create",
-            context: "POST",
-            message: "PSA records not valid",
-            code: "401",
+      if (!dataSource || !site || !agent) {
+        return Debug.response(
+          {
+            error: {
+              module: "v1.0/ticket/create",
+              context: "POST",
+              message: "PSA records not valid",
+              code: "401",
+            },
           },
-        },
-        404
-      );
-    }
+          404
+        );
+      }
 
-    const connector = new HaloPSAConnector(dataSource.config as HaloPSAConfig);
-    const { data: assets } = await connector.getAssets(
-      site?.psa_company_id || ""
-    );
-    if (!assets) {
-      return Debug.response(
-        {
-          error: {
-            module: "v1.0/ticket/create",
-            context: "POST",
-            message: "Failed to fetch halo assets",
-            code: "404",
+      Debug.log({
+        module: "v1.0/ticket/create",
+        context: "POST",
+        message: `Creating ticket for agent ${agent.hostname} (DeviceID: ${agent.id}) (SiteID: ${siteID})`,
+      });
+
+      const connector = new HaloPSAConnector(
+        dataSource.config as HaloPSAConfig
+      );
+      const { data: assets } = await connector.getAssets(
+        site?.psa_company_id || ""
+      );
+      if (!assets) {
+        return Debug.response(
+          {
+            error: {
+              module: "v1.0/ticket/create",
+              context: "POST",
+              message: "Failed to fetch halo assets",
+              code: "404",
+            },
           },
-        },
-        404
-      );
-    }
+          404
+        );
+      }
 
-    const body = JSON.parse(req.body as string) as {
-      screenshot?: {
+      Debug.log({
+        module: "v1.0/ticket/create",
+        context: "POST",
+        message: `Found ${assets.length} HaloPSAAssets (HaloSiteID: ${site.psa_company_id})`,
+      });
+
+      const body = JSON.parse(req.body as string) as {
+        screenshot?: {
+          name?: string;
+          data?: string;
+        };
+        link?: string;
+        summary: string;
+        description?: string;
+
         name: string;
-        data: string;
+        email: string;
+        phone: string;
+
+        impact: string;
+        urgency: string;
+
+        rmm_id?: string;
       };
-      link?: string;
-      summary: string;
-      description?: string;
 
-      name: string;
-      email: string;
-      phone: string;
+      const asset = assets.find((a) => {
+        if (body.rmm_id) {
+          return a.datto_id === body.rmm_id;
+        }
 
-      impact: string;
-      urgency: string;
+        return a.inventory_number === agent.hostname;
+      });
 
-      rmm_id?: string;
-    };
-
-    const asset = assets.find((a) => {
-      if (body.rmm_id) {
-        return a.datto_id === body.rmm_id;
+      if (asset) {
+        Debug.log({
+          module: "v1.0/ticket/create",
+          context: "POST",
+          message: `HaloAsset found for ${agent.hostname} (HaloID: ${asset?.id})`,
+        });
       }
 
-      return a.inventory_number === agent.hostname;
-    });
+      if (body.screenshot && body.screenshot.data && body.screenshot.name) {
+        const binary = atob(body.screenshot.data);
+        const len = binary.length;
+        const bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+          bytes[i] = binary.charCodeAt(i);
+        }
 
-    if (body.screenshot) {
-      const binary = atob(body.screenshot.data);
-      const len = binary.length;
-      const bytes = new Uint8Array(len);
-      for (let i = 0; i < len; i++) {
-        bytes[i] = binary.charCodeAt(i);
+        const blob = new Blob([bytes], { type: "image/png" });
+        const { data } = await connector.uploadImage(blob);
+        if (data) {
+          body.link = data;
+
+          Debug.log({
+            module: "v1.0/ticket/create",
+            context: "POST",
+            message: `Image uploaded to HaloPSA for ${agent.hostname} (Link: ${body.link})`,
+          });
+        }
       }
 
-      const blob = new Blob([bytes], { type: "image/png" });
-      const { data } = await connector.uploadImage(blob);
-      if (data) body.link = data;
-    }
+      const { data: ticketID } = await connector.createTicket({
+        siteId: Number(site.psa_company_id),
+        clientId: Number(site.psa_parent_company_id),
+        summary: body.summary,
+        details: body.description || "",
+        user: {
+          name: body.name,
+          email: body.email,
+          phone: body.phone,
+        },
+        impact: body.impact,
+        urgency: body.urgency,
+        assets: asset ? [asset.id] : [], // TODO: Find assets,
+        images: body.link ? [body.link] : [],
+      });
 
-    const { data: ticketID } = await connector.createTicket({
-      siteId: Number(site.psa_company_id),
-      clientId: Number(site.psa_parent_company_id),
-      summary: body.summary,
-      details: body.description || "",
-      user: {
-        name: body.name,
-        email: body.email,
-        phone: body.phone,
-      },
-      impact: body.impact,
-      urgency: body.urgency,
-      assets: asset ? [asset.id] : [], // TODO: Find assets,
-      images: body.link ? [body.link] : [],
-    });
+      if (!ticketID) {
+        return Debug.response(
+          {
+            error: {
+              module: "v1.0/ticket/create",
+              context: "POST",
+              message: "Failed to create ticket",
+              code: "500",
+            },
+          },
+          500
+        );
+      }
 
-    if (!ticketID) {
+      Debug.log({
+        module: "v1.0/ticket/create",
+        context: "POST",
+        message: `Ticket create in HaloPSA for ${agent.hostname} (TicketID: ${ticketID})`,
+      });
+
+      return Debug.response(
+        {
+          data: ticketID,
+        },
+        200
+      );
+    } catch (err) {
       return Debug.response(
         {
           error: {
             module: "v1.0/ticket/create",
             context: "POST",
-            message: "Failed to create ticket",
+            message: `Failed to create ticket: ${err}`,
             code: "500",
           },
         },
-        404
+        500
       );
     }
-
-    return Debug.response(
-      {
-        data: ticketID,
-      },
-      200
-    );
   });
 }
