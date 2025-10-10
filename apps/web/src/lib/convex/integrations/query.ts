d8d5d12b4a05cd268d381e8c973ffdef9787f8c9
Mythidas <root@mythidas.com>
2025-10-09 19:55:26 -0500

    Convex migration start, integrations working with simplified impl

--- apps/web/src/lib/convex/integrations/query.ts
+++ apps/web/src/lib/convex/integrations/query.ts
@@ -1,31 +1,29 @@
-import { query } from "../_generated/server.js";
-import { isAuthenticated } from "../helper.js";
+import { query } from '../_generated/server.js';
+import { isAuthenticated } from '../helper.js';
 
 export const getActiveIntegrationsView = query({
-  args: {},
-  handler: async (ctx, args) => {
-    const identity = await isAuthenticated(ctx);
+	args: {},
+	handler: async (ctx, args) => {
+		const identity = await isAuthenticated(ctx);
 
-    const integrations = await ctx.db
-      .query("integrations")
-      .withIndex("by_is_active")
-      .collect();
+		const integrations = await ctx.db.query('integrations').withIndex('by_is_active').collect();
 
-    return Promise.all(
-      integrations.map(async (integration) => {
-        const dataSource = await ctx.db
-          .query("data_sources")
-          .withIndex("by_integration_primary", (q) =>
-            q.eq("integrationId", integration._id).eq("isPrimary", true)
-          )
-          .unique();
+		return Promise.all(
+			integrations.map(async (integration) => {
+				const dataSource = await ctx.db
+					.query('data_sources')
+					.withIndex('by_integration_primary', (q) =>
+						q.eq('integrationId', integration._id).eq('isPrimary', true)
+					)
+					.unique();
 
-        return {
-          ...integration,
-          isEnabled: (dataSource && !dataSource.deletedAt) || false,
-          dataSourceId: dataSource?._id,
-        };
-      })
-    );
-  },
+				return {
+					...integration,
+					isEnabled: (dataSource && !dataSource.deletedAt) || false,
+					dataSourceId: dataSource?._id,
+					dataSourceStatus: dataSource?.status
+				};
+			})
+		);
+	}
 });
