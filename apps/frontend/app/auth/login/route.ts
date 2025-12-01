0b2ebefe4a355c4ffdc56709303942b7491aaed2
Mythidas <root@mythidas.com>
2025-12-01 11:50:24 -0600

    Add simple landing page

--- apps/frontend/app/auth/login/route.ts
+++ apps/frontend/app/auth/login/route.ts
@@ -3,7 +3,6 @@ import { getSignInUrl } from '@workos-inc/authkit-nextjs';
 
 export async function GET() {
     const authorizationUrl = await getSignInUrl({
-        redirectUri: `${process.env.NEXT_PUBLIC_ORIGIN}/secure`
     });
     return redirect(authorizationUrl);
 }
